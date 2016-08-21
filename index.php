7c52d10deaa3d7f90335b5cb51445fa5aa2e4933
Bartosz W <admin@isvn.pl>
2016-08-21 19:15:37 +0200

    Wrzucenie aktualnej wersji

---
+++ index.php
@@ -0,0 +1,119 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script type="text/javascript" src="//www.google.com/recaptcha/api.js"></script>
+  </head>
+<body>
+
+
+
+<style>
+body, html {margin:0;padding:0;}
+#contact_form {
+  width: 600px;;
+  padding: 50px;
+  margin: 25px auto;
+  background: white;
+  background: url('https://zotabox.com/media/core/image/gallery/simplepopup/1d.jpg');
+  background-size: cover;
+  box-shadow: 0px 0px 30px -5px rgba(0,0,0,0.5);
+} #contact_form div.flex {
+  display: flex;
+  width: 100%;
+} #contact_form div.field {
+  position: relative;
+  width: 100%;
+  margin: 5px;
+} #contact_form div.field input, #contact_form div.field textarea {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  padding-left: 30px;
+  outline: none;
+  border: 1px solid rgba(0,0,0,0.2);
+  border-radius: 4px;
+  background: white;
+  font-family: Arial;
+  color: black;
+} #contact_form div.field input:hover, #contact_form div.field input:focus,
+#contact_form div.field textarea:hover, #contact_form div.field textarea:focus {
+  border: 1px solid rgba(0,0,0,0.4);
+} #contact_form div.field #submit_button {
+  width: auto;
+} #contact_form div.field #submit_button:hover {
+  cursor: pointer;
+} #contact_form div.field .icon {
+  position: absolute;
+  top: 9px;
+  left: 7px;
+  color: rgba(0,0,0,0.5);
+  cursor: text;
+} #contact_form div.field input:hover + .icon, #contact_form div.field input:focus + .icon,
+#contact_form div.field textarea:hover + .icon, #contact_form div.field textarea:focus + .icon {
+  color: rgba(0,0,0,0.7);
+}
+</style>
+
+<form id="contact_form" method="post" action="submit.php">
+  <div class="flex">
+    <div class="field">
+      <input name="name_field" id="name_field" placeholder="Twój nick lub imię">
+      <label class="icon" for="name_field">
+        <i class="fa fa-user fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+    <div class="field">
+      <input name="address_field" id="address_field" placeholder="Twój adres e-Mail">
+      <label class="icon" for="address_field">
+        <i class="fa fa-envelope fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+  </div>
+  <div class="flex">
+    <div class="field" style="display: none;">
+      <input name="phone_field" id="phone_field" placeholder="Twój numer telefonu">
+      <label class="icon" for="phone_field">
+        <i class="fa fa-phone fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+    <div class="field"  style="display: none;">
+      <input name="company_field" id="company_field" placeholder="Firma">
+      <label class="icon" for="company_field">
+        <i class="fa fa-briefcase fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+  </div>
+  <div class="flex">
+    <div class="field">
+      <input name="subject_field" id="subject_field" placeholder="Temat">
+      <label class="icon" for="subject_field">
+        <i class="fa fa-star fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+  </div>
+  <div class="flex">
+    <div class="field">
+    <textarea name="message_field" id="message_field" placeholder="Tutaj wpisz treść swojej wiadomości..." rows="7" maxlength="1000" spellcheck="true"></textarea>
+      <label class="icon" for="message_field">
+        <i class="fa fa-pencil fa-fw" aria-hidden="true"></i>
+      </label>
+    </div>
+  </div>
+  <div class="flex">
+    <div class="field">
+      <div class="g-recaptcha" data-sitekey="6LfnGigTAAAAAGsECHWJRKpajpJI0SYt24XFy8S-"></div>
+    </div>
+    <div class="field" style="text-align: right;"><div style="display: inline-block; position: relative;">
+    <input id="submit_button" type="submit" value="Wyślij">
+      <label class="icon" for="submit_button" style="cursor: pointer;">
+        <i class="fa fa-paper-plane fa-fw" aria-hidden="true"></i>
+      </label>
+    </div></div>
+  </div>
+</form>
+
+
+
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,78 @@
+<?php
+
+function died($error) {
+    echo '<p><h2>Wystąpił problem z działaniem skryptu.</h2></p>';
+    echo '<p>Wykryto następujące błędy:<ul>'.$error.'</ul></p>';
+    echo '<p>Wiadomośc nie została wysłana.</p>';
+    echo '<p><a href="javascript:history.go(-1)">Kliknij tutaj</a>, aby wrócić i spróbować ponownie.</p>';
+    //echo '<p><a href="javascript:window.close()">Kliknij tutaj</a>, aby zamknąć kartę i spróbuj ponownie..</p>';
+    die();
+};
+
+function done() {
+  echo '<p><h1>Wiadomość została wysłana.</h1></p>';
+}
+
+if ( isset($_POST) ) {
+
+  // Adresat
+  $do         = 'idolwszutrab7@gmail.com'; // Adres e-Mail na który ma zostać wysłany formularz
+
+  // Dane formularza
+  $imie       = $_POST['name_field'];
+  $email      = $_POST['address_field'];
+  $telefon    = $_POST['phone_field'];
+  $firma      = $_POST['company_field'];
+  $temat      = $_POST['subject_field'];
+  $wiadomosc  = $_POST['message_field'];
+
+  $captcha    = $_POST['g-recaptcha-response'];
+
+  // Nagłówek wiadomości e-Mail
+  $headers = array('From: "'.$imie.'" <'.$email.'>',
+                   'Reply-To: '.$email,
+                   'X-Mailer: PHP/' . PHP_VERSION);
+  $headers = implode("\r\n", $headers);
+
+  // ReCaptcha NoCaptcha
+  $secret = "6LfnGigTAAAAAKpFgE0kSzkMlBHzqi9EuF-6yjub";
+  if (!$captcha) {
+    died('<li>Proszę wypełnić formularz reCAPTCHA.</li>');
+    exit;
+  }
+  $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+
+  // Wytyczne do sprawdzenia danych
+  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  $imie_exp = "/^[A-Za-z ęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+$/";
+  $telefon_exp = "/^[0-9 ]+$/";
+  
+  // Sprawdzanie formularza w poszukiwaniu błędów
+  if ($response['success'] == false) { // Tutaj sprawdza, czy reCAPtCHa zwraca success
+    died('<li>Walidacja reCAPTCHA nie przeszła poprawnie!<br />Twoje działanie zostało zarejestrowane, a dane przesłane do administratora.</li>');
+    exit;
+  } else if (  !isset($_POST['name_field'])    ||
+               !isset($_POST['address_field']) ||
+               !isset($_POST['message_field']) ) { // Tutaj jest bloczek, który sprawdza czy dane pola są uzupełnione
+    died('<li>Nie wszystkie wymagane pola zostały wypełnione.</li>');       
+  } else if ( !preg_match($email_exp,$email) ) { // Tutaj sprawdza, czy e-Mail jest poprawnie uzupełniony
+    died('<li>Adres e-Mail nie wygląda na autentyczny.</li>');
+  } else if ( !preg_match($imie_exp,$imie) ) { // Tutaj sprawdza, czy Imię lub Nick nie zawiera jakichś dziwnych znaków
+    died('<li>Imię lub Nick zawiera nienaturalne znaki.</li>');
+  } else if ( !preg_match($telefon_exp,$telefon) && !$telefon == null ) { // Tutaj sprawdza, czy telefon zawiera cyfry tylko wtedy, gdy jest coś wpisane [inaczej widzi pole ktore jest ukryte i trkatuje je jako blad]
+    died('<li>Numer telefonu powinien zawierać wyłącznie cyfry.');
+  } else if ( strlen($wiadomosc) < 10 || strlen($wiadomosc)>1000 ) { // Tutaj sprawdza długość wiadomości
+    died('<li>Wiadomość jest zbyt krótka lub zbyt długa.');
+  } else if ( strlen($imie)>50    ||
+              strlen($email)>50   ||
+              strlen($telefon)>50 ||
+              strlen($firma)>50   ||
+              strlen($temat)>100 ) { // Tutaj jest bloczek, który sprawdza długość poszczególnych pól
+    died('<li>Przekroczono maksymalną ilość znaków w niektórych polach.</li>');
+  } else { done(); mail($do, $temat, $wiadomosc, $headers); };
+
+} else { 
+  died('<li>Nastąpiło nieoczekiwane zatrzymanie skryptu!</li>');
+};
+
+?>
